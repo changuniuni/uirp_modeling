28b210e3178780b404f28b6c7d92a8985e802ebc
ChangHyeon Kim <48553632+changuniuni@users.noreply.github.com>
2022-11-10 20:32:03 +0900

    11/10 meeting

--- tcell.jl
+++ tcell.jl
@@ -1,47 +1,64 @@
 using Agents
 using Random
 
-
-mutable struct dcell <: AbstractAgent
-    pos :: Tuple{float, float}
+@agent tcell ContinuousAgent{2} begin
     speed::Float64
     activated::Bool
+    visual_distance::Float64
 end
 
-mutable struct tcell <: AbstractAgent
-    pos::Tuple{float, float}
+@agent dcell ContinuousAgent{2} begin
     speed::Float64
     activated::Bool
+    visual_distance::Float64
 end
-
 
 
 function initialize_model(; 
     n_dcell = 5, 
     n_tcell = 10, 
-    speed = 1.0, 
-    extent = (100,100),    
+    speed = 5.0,
+    extent = (300,300),    
+    visual_distance = 5.0,
+    activated_status= false,
+    tuple = (1.0,1.0),
     )
 
-    space2d = ContinuousSpace(extent; spacing = 3.0)
+    space2d = ContinuousSpace(extent; spacing = 3)
     
-    model = ABM(Union{dcell, tcell}, space2d),  scheduler = Schedulers.Randomly()
-
-    # model_dcell = ABM(DCell, space2d, scheduler = Schedulers.Randomly())
-    # model_tcell = ABM(TCell, space2d,  scheduler = Schedulers.Randomly())
+    cells = Union{tcell, dcell}
+    model = ABM(cells, space2d ; 
+        scheduler = Schedulers.randomly)
 
     for _ in 1:n_tcell
         add_agent!(
-            tcell, model, speed
+            tcell, model, tuple, speed, activated_status, visual_distance
         )
     end
     
     for _ in 1:n_dcell
         add_agent!(
-            dcell, model, speed
+            dcell, model, tuple, speed, activated_status, visual_distance,
         )
     end
+
     return model
 end
+
+a = initialize_model()
+
+# function agent_step!(cells, model)
+#     neighbot_ids = nearby_ids(cells, model, cells.visual_distance)
+#     N = 0
+# end
+
+    
+    # for id in nearby_ids
+    #     neighbor = model[id].pos
+    #     heading  = neighbor .- cells.pos
+
+
+
+
 
 
